cd926a8e5a638c4cdf1c8d89a70f1dee9b207add
Panttersson <Gabrielhas2001@gmail.com>
2022-11-02 09:22:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+//Variaveis
 let musica = document.querySelector('audio');
 
+// Lista de Eventos do JavaScript
 document.querySelector('.botao-play').addEventListener('click', tocarMusica);
 document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
+musica.addEventListener('timeupdate', atualizarBarra);
 
+
+// Lista de funções
 function tocarMusica() {
     musica.play();
     document.querySelector('.botao-pause').style.display = 'block';
@@ -13,4 +18,11 @@ function pausarMusica() {
     musica.pause();
     document.querySelector('.botao-play').style.display = 'block';
     document.querySelector('.botao-pause').style.display = 'none';
+}
+
+function atualizarBarra() {
+    let barra = document.querySelector('progress');
+    barra.style.width = Math.floor(( musica.currentTime / musica.duration) * 100) + '%';
+    let tempoDecorrido = document.querySelector('.inicio');
+    tempoDecorrido.textContent = Math.floor(musica.currentTime);
 }
